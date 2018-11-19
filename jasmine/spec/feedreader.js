83b8d9e79bca116c9f7684c98d33c02b516e499b
Mohit Sharma <Mohit.Sharma12@aexp.com>
2018-11-19 15:35:24 -0500

    chore(Testing) written all the test cases

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,42 +31,102 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('have valid defined URLs and not empty', function() {
+            allFeeds.forEach(function(x) {
+                expect(x.url).toBeDefined();
+                expect(x.url.length).toBeGreaterThan(1);
+            });
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have valid defined names and not empty', function() {
+            allFeeds.forEach(function(x) {
+                expect(x.name).toBeDefined();
+                expect(x.name.length).toBeGreaterThan(1);
+            });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
+        var body = $('body');
+        var menuIcon = $('.menu-icon-link');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function() {
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+        it('icon visibility changes on click', function() {
+            menuIcon.trigger('click');
+            expect(body.hasClass('menu-hidden')).toBe(false);
+            menuIcon.trigger('click');
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        });
+    });
+
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('have at least a single entry', function(done) {
+            var entry = $('.feed').find('.entry');
+            expect(entry.length >= 1).toBe(true);
+            done();
+        });
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        //Initializing pre and post variables for setting the contents of the feed
+        var prevEntries;
+        var newEntries;
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            // Loading the feed at 0
+            loadFeed(0, function() {
+                /* Sets for before in the feed */
+                prevEntries = $('.header-title').text() + $('.feed').find('.entry').text().replace(/ +/g, " ");
+                /* Load second feed at index 1 */
+                loadFeed(1, function() {
+                    /* Sets for after in the feed */
+                    newEntries = $('.header-title').text() + $('.feed').find('.entry').text().replace(/ +/g, " ");
+                    done();
+                });
+            });
+        });
+
+        it('the content changes with new feed entry', function(done) {
+            expect(prevEntries != newEntries).toBe(true);
+            done();
+        });
+    });
 }());
